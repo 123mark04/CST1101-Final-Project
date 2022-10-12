1c7792139c3198ad2544b5e1e7ff65421fa887df
123mark04 <63602640+123mark04@users.noreply.github.com>
2022-10-12 16:40:26 +0000

    add variables for player

---
+++ main.py
@@ -0,0 +1,9 @@
+#These are the stats from prof tams template im gonna change some of them. ~mark
+#Its possible that we can do it like one of those games for example fallout where you get a certain amount of points you can use on personality. Like you get 10 points to use on the characteristics you want.
+#btw replit has a chat feature use that instead of commenting whole paragraphs into the code.
+
+Trust = 50
+Charisma = 50
+IQ = 50
+
+
