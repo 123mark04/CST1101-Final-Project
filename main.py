4db9e52be590c825e657fcd94a9599c4e686a4f6
123mark04 <63602640+123mark04@users.noreply.github.com>
2022-10-12 16:48:48 +0000

    added name choice

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 #Its possible that we can do it like one of those games for example fallout where you get a certain amount of points you can use on personality. Like you get 10 points to use on the characteristics you want.
 #btw replit has a chat feature use that instead of commenting whole paragraphs into the code.
 
-Trust = 50
-Charisma = 50
-IQ = 50
+#SP stands for Stat Points.
+SP = 10
 
+Trust = 0
+Charisma = 0
+IQ = 0
 
+name = input("What is your name player?").lower()
+print("Ah yes", name, "what a wonderful name!")
