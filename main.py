b34aecab139cbe88126b4524e908fd7928e3080f
123mark04 <63602640+123mark04@users.noreply.github.com>
2022-10-13 16:58:21 +0000

    added some delays between prints

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-#These are the stats from prof tams template im gonna change some of them. ~mark
+#These are the stats from prof tams template im gonna change some of them.
 #Its possible that we can do it like one of those games for example fallout where you get a certain amount of points you can use on personality. Like you get 10 points to use on the characteristics you want.
 #btw replit has a chat feature use that instead of commenting whole paragraphs into the code.
 
-#SP stands for Stat Points.
-
+#SP stands for Stat Points. ~mark
+import time
 SP = 10
 
 Trust = 0
 Charisma = 0
 IQ = 0
 
-name = input("What is your name player?").lower()
-print("Ah yes", name, "what a wonderful name!")
+name = input("""What is your name player?
+""").lower()
 
-print("""Now time to choose your stats!
-You have 10 points to use on which ever status you like.
-But be warned what is done cannot be undone!""")
+
+#I thought having it too instant was kinda weird looking so i added some delays
+print("Ah yes", name, "what a wonderful name!")
+time.sleep(1.5)
+print("Now time to choose your stats!")
+time.sleep(1.5)
+print("You have 10 points to use on which ever status you like.")
+time.sleep(1.5)
+print("But be warned what is done cannot be undone!")
+time.sleep(2)
+#import time
+#time.sleep(5)
+#print("test")
+#^^^^^^^^^^^^^
+#I googled how to set delays for text and you just do that.~mark
+
+print("Current Status")#stats gonna go here
