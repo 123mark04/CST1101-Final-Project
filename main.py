c08f3a0912b5ff517953c6134d70a371a9480ed4
123mark04 <63602640+123mark04@users.noreply.github.com>
2022-10-17 17:54:55 +0000

    Removed SP

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import time
-SP = 10
 
-Trust = 0
+
 Charisma = 0
-IQ = 0
+IQ = 50
 attack = 20
 defense = 20
 fire = 0
@@ -11,16 +10,15 @@ water = 0
 earth = 0
 wind = 0
 def intro():
-  global Trust
   global Charisma
-  global IQ 
-  global attack 
+  global IQ  
   global defense 
   global fire
   global water
   global earth
   global wind 
-
+  global mainAbility
+  global mainScore
   name = input("What is your name player?")
   time.sleep(1)
   print("Ah yes " + name + ". what a wonderful name!")
@@ -32,45 +30,84 @@ def intro():
   print("But be warned what is done cannot be undone!")
   time.sleep(1.5)
 
-  mainAbility = input("You can choose fire, water, earth, or wind?")
+  mainAbility = input("You can choose fire, water, earth, or wind?").lower()
 
   while True:
     if mainAbility == "fire":
       fire += 10
+      mainScore = fire
       break
     elif mainAbility == "water":
       water += 10
+      mainScore = water
       break
     elif mainAbility == "earth":
       earth += 10
+      mainScore = earth
       break
     elif mainAbility == "wind":
       wind += 10
+      mainScore = wind
       break
     else:
-      mainAbility = input("Invalid Response. Please choose fire, water, earth or wind.")
+      mainAbility = input("""I didn't quite get that. You can only pick fire, water, earth or wind so choose wisely.""").lower()
   time.sleep(1.5)
 
-  print("Great chioce! Now it is time to start the adventure!")
+  print("""Great chioce! Now it is time to start the adventure!""")
   time.sleep(1.5)
   
 def story1():
-  global Trust
+  print("""\nYour journey begins in a country called Vextop ruled by an evil tyrant named King Leonardo.""")
+  time.sleep(4)
+  
+  print("""\nThe king heavily exploits the commoners for cheap labor. They make up 60% of the population and have low magical abilities. The nobles are people with great magical power. They use their power to take advantage of the commoners and makeup 10% of the population. The rest of the population is the middle class who live a somewhat peaceful life.""")
+  time.sleep(10)
+  
+  print("""\nYou unfortunately live in an orphanage with commoners, but you possess great potential. You hope to liberate the people of Vextop by killing the king.""")
+  time.sleep(6)
+
+def story2():
   global Charisma
   global IQ 
-  global attack 
   global defense 
   global fire
   global water
   global earth
   global wind
-  print("You have now been spawned into a kingdom of free for all.")
-  time.sleep(1.5)
-  print("The main objective is to aquire the stone of power before anyone else.")
-  time.sleep(1.5)
-  print("However, you might go through some obsticles on your way to getting the stone.")
-  time.sleep(1.5)
-  print("syed its been like 2 hours im done ima go to bed gn")
+  global mainAbility
+  global mainScore
+  choice1 = input("""\nYou are currently 7 years olds. Some of orpans spend their free time training their magic or read books to gain knowledge. What would you like to do?)
+  
+  A. Train with your friends to increase your """ +str(mainAbility)+""" mastery.
+  
+  B. Learn how to read difficult books to increase your knowledge.""").lower()
+  
+  while True:
+    if choice1 == "a":
+      mainScore += 30
+      defense += 10
+      time.sleep(1)
+      print("""\nYou trained long and hard everyday with your friends. You kept training for 3 years when ever you found time. You got a few bruises and scars but it helpped define your mucules and made you stronger.""")
+      break
+    elif choice1 == "b":
+      IQ += 30
+      defense += 10
+      time.sleep(1)
+      print("""\nWith great knowledge comes great responsibility. You are now able to read a variety amount of books and know how to speak formaly. You spend 3 years studing you learned the ability the read people and effective battle stategies during conflict.""")
+      break
+    else:
+      choice1 = input("You must choose option A or B. So pick one! ")
+  time.sleep(7)
+
+  print("""\nYou decide that it is about time leave the orphanage and start to travel through the country to gains new skills and alies. Before leave you ask everyone to if they would join you on your quest.""")
+  time.sleep(8)
+  if choice1 == "a":
+    print("""\nEveryone looked at each other unsure wheather to join you. But one person name Michael agreed to join you. He was your best friend and you two train a lot with each other. You were happy to have a companion and set off to a city called Fargu.""")
+    time.sleep(5)
+  else:
+    print("""\nEveryone looked at each other and they all declined. They were all afraid and didn't believe that you can kill the king one day. I guess it is the consequese of reading all day and not making friends. None the less you go off on your quest alone to your first city called Fargu.""")
+    time.sleep(5)
 
 intro()
 story1()
+story2()
